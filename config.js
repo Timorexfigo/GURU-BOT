3dde12488a6a5d26a206aec679740dd160ef17c0
Timorexfigo <157409975+Timorexfigo@users.noreply.github.com>
2024-02-19 20:13:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348087558037', 'king', true],
+  ['2348087558037', 'mubzy', true], 
   [''] 
 ] //Number of owners
 
